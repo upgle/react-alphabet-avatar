44358303d2aed557265650badeb3becd1cc9ded5
UPGLE <admin@upgle.com>
2015-07-22 20:28:46 +0900

    add colorset background and text.

--- lib/react-alphabet-avatar.jsx
+++ lib/react-alphabet-avatar.jsx
@@ -7,7 +7,7 @@ var ReactAlphabetAvatar = React.createClass({
         height: React.PropTypes.number,
         border_radius: React.PropTypes.number,
         colorset: React.PropTypes.array,
-        user_name: React.PropTypes.string
+        text: React.PropTypes.string.isRequired
     },
 
     getDefaultProps: function(){
@@ -15,32 +15,52 @@ var ReactAlphabetAvatar = React.createClass({
             width: 100,
             height: 100,
             border_radius: 0
+            text: 'A',
+            colorset: ['#ed7872', '#5e97f6', '#ba68c8', '#66cccc', '#fbc02d', '#90a4ae', '#8d6e63', '#6492ce']
         };
     },
 
+    getColorset: function(alphabet) {
+        return this.props.colorset[this.getStringHashCode(alphabet) % this.props.colorset.length];
+    },
+
+    getStringHashCode: function(string) {
+        var hash = 0, i, chr, len;
+        if (string.length == 0) return hash;
+        for (i = 0, len = string.length; i < len; i++) {
+            chr   = string.charCodeAt(i);
+            hash  = ((hash << 5) - hash) + chr;
+            hash |= 0; // Convert to 32bit integer
+        }
+        return hash;
+    },
+
     shouldComponentUpdate: function(nextProps) {
-        return this.props.user_name !== nextProps.user_name;
+        return this.props.text !== nextProps.text;
     },
 
     render: function(){
 
-        var fontSize = Math.round((this.props.height + this.props.width) / 4);
+        var alphabet = this.props.text.substring(0, 1),
+            fontSize = Math.round((this.props.height + this.props.width) / 4);
+
+        this.getColorset(alphabet);
 
         var divStyle = {
+            margin: 0,
+            padding: 0,
             color: 'white',
             height: this.props.height,
             width: this.props.width,
             lineHeight: this.props.height + 'px',
             textAlign: 'center',
-            background: '#dddddd',
+            background: this.getColorset(alphabet),
             borderRadius: this.props.border_radius,
             fontSize: fontSize
         };
 
         return (
-            <div style={divStyle}>
-                G
-            </div>
+            <div style={divStyle}>{alphabet}</div>
         );
     }
 
